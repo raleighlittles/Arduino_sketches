58b5263777d49752f27a063c6b862cae26d91a95
Raleigh Littles III <raleighlittles@gmail.com>
2023-07-27 16:05:24 -0700

    now works with extended keypad input

--- MT8870_dtmf_decoder/mt8870_dtmf_decoder.ino
+++ MT8870_dtmf_decoder/mt8870_dtmf_decoder.ino
@@ -1,60 +1,83 @@
 
+//// ----- Pin names ----- ////
+const int PIN_Q1 = 3;
+const int PIN_Q2 = 4;
+const int PIN_Q3 = 5;
+const int PIN_Q4 = 6;
+//// Delayed Steering output - called 'STD' in Datasheet
+const int PIN_STQ = 7;
+
 void setup()
 {
     Serial.begin(9600);
-    pinMode(3, INPUT);
-    pinMode(4, INPUT);
-    pinMode(5, INPUT);
-    pinMode(6, INPUT);
-    pinMode(7, INPUT);
+    pinMode(PIN_Q1, INPUT);
+    pinMode(PIN_Q2, INPUT);
+    pinMode(PIN_Q3, INPUT);
+    pinMode(PIN_Q4, INPUT);
+    pinMode(PIN_STQ, INPUT);
 }
 
 void loop()
 {
     uint8_t number;
-    bool signal;
-    signal = digitalRead(7);
-    if (signal == HIGH) /*When DTMF tone is detected, STQ will read HIGH for the duration of the tone*/
+
+    const bool output_signal = digitalRead(PIN_STQ);
+
+    if (output_signal == HIGH) /*When DTMF tone is detected, STQ will read HIGH for the duration of the tone*/
     {
         delay(100);
+
         number = (0x00 | (digitalRead(3) << 0) | (digitalRead(4) << 1) | (digitalRead(5) << 2) | (digitalRead(6) << 3));
+
         switch (number)
-        {
+        { // Comes from Table 1 - Functional Decode Table
         case 0x01:
-            Serial.println("Pin Pressed: 1");
+            Serial.print("1");
             break;
         case 0x02:
-            Serial.println("Pin Pressed: 2");
+            Serial.print("2");
             break;
         case 0x03:
-            Serial.println("Pin Pressed: 3");
+            Serial.print("3");
             break;
         case 0x04:
-            Serial.println("Pin Pressed: 4");
+            Serial.print("4");
             break;
         case 0x05:
-            Serial.println("Pin Pressed: 5");
+            Serial.print("5");
             break;
         case 0x06:
-            Serial.println("Pin Pressed: 6");
+            Serial.print("6");
             break;
         case 7:
-            Serial.println("Pin Pressed: 7");
+            Serial.print("7");
             break;
         case 0x08:
-            Serial.println("Pin Pressed: 8");
+            Serial.print("8");
             break;
         case 0x09:
-            Serial.println("Pin Pressed: 9");
+            Serial.print("9");
             break;
         case 0x0A:
-            Serial.println("Pin Pressed: 0");
+            Serial.print("0");
             break;
         case 0x0B:
-            Serial.println("Pin Pressed: *");
+            Serial.print("*");
             break;
         case 0x0C:
-            Serial.println("Pin Pressed: #");
+            Serial.print("#");
+            break;
+        case 0x0D:
+            Serial.print("A");
+            break;
+        case 0x0E:
+            Serial.print("B");
+            break;
+        case 0x0F:
+            Serial.print("C");
+            break;
+        case 0x00:
+            Serial.print("D");
             break;
         }
     }
